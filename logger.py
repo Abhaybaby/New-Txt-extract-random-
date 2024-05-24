467483b5d4efb7baac2325a8a0635e2eb7878f0c
Abhaybaby <167125342+Abhaybaby@users.noreply.github.com>
2024-05-24 12:39:21 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,18 +1,18 @@
-import logging   #Bot Created by @NtrRazYt
-from logging.handlers import RotatingFileHandler   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-logging.basicConfig(   #Bot Created by @NtrRazYt
-    level=logging.ERROR,   #Bot Created by @NtrRazYt
-    format=   #Bot Created by @NtrRazYt
-    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",   #Bot Created by @NtrRazYt
-    datefmt="%d-%b-%y %H:%M:%S",   #Bot Created by @NtrRazYt
-    handlers=[   #Bot Created by @NtrRazYt
-        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),   #Bot Created by @NtrRazYt
-        logging.StreamHandler(),   #Bot Created by @NtrRazYt
-    ],   #Bot Created by @NtrRazYt
-)   #Bot Created by @NtrRazYt
-logging.getLogger("pyrogram").setLevel(logging.WARNING)   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-logging = logging.getLogger()   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
+import logging   #Bot Created by @REACHING_STAR_OWNER
+from logging.handlers import RotatingFileHandler   #Bot Created by @REACHING_STAR_OWNER
+   #Bot Created by @REACHING_STAR_OWNER
+logging.basicConfig(   #Bot Created by @REACHING_STAR_OWNER
+    level=logging.ERROR,   #Bot Created by @REACHING_STAR_OWNER
+    format=   #Bot Created by @REACHING_STAR_OWNER
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",   #Bot Created by @REACHING_STAR_OWNER
+    datefmt="%d-%b-%y %H:%M:%S",   #Bot Created by @REACHING_STAR_OWNER
+    handlers=[   #Bot Created by @REACHING_STAR_OWNER
+        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),   #Bot Created by @REACHING_STAR_OWNER
+        logging.StreamHandler(),   #Bot Created by @REACHING_STAR_OWNER
+    ],   #Bot Created by @REACHING_STAR_OWNER
+)   #Bot Created by @REACHING_STAR_OWNER
+logging.getLogger("pyrogram").setLevel(logging.WARNING)   #Bot Created by @REACHING_STAR_OWNER
+   #Bot Created by @REACHING_STAR_OWNER
+   #Bot Created by @REACHING_STAR_OWNER
+logging = logging.getLogger()   #Bot Created by @REACHING_STAR_OWNER
+   #Bot Created by @REACHING_STAR_OWNER
